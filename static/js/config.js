1a3bb51e9dbacc02f041758ce157c671bc92f087
Phu Tran <tr.ngocphu@gmail.com>
2020-04-23 22:41:25 +0800

    add a background layer

--- static/js/config.js
+++ static/js/config.js
@@ -9,20 +9,34 @@
 	65 Nanyang Drive, Nanyang Technological University,
 	North Spine, Block N3.2-B3M-10, 637460 Singapore
  * -----
- * Last Modified: Thursday, 23rd April 2020 10:31:00 pm
+ * Last Modified: Thursday, 23rd April 2020 10:39:33 pm
  * Modified By: Phu N. Tran
  * -----
  */
 
 
 /** GLOBAL CONSTANTS */
-const PAPER_SIZE = 800;
+const PAPER_SIZE = 900;
 const PAPER_WIDTH  = PAPER_SIZE;
 const PAPER_HEIGHT = PAPER_SIZE;
+const BG_COLOR = "#292929";
  
 
 /*************************************************/
+
+// Paper setup
 let canvas = document.getElementById('myCanvas');
 canvas.width = PAPER_WIDTH;
 canvas.height = PAPER_HEIGHT;
 view.Size = [PAPER_WIDTH, PAPER_HEIGHT];
+
+
+// Add a rectangle as a bg layer
+let bg = new Path.Rectangle({
+    point: [0, 0],
+    size: [PAPER_WIDTH, PAPER_HEIGHT],
+    fillColor: BG_COLOR,
+    strokeColor: '',
+    strokeWidth: 0,
+    visible: true
+});
