3dd4b7a76c6d425618fcea4f491a493850de6f7d
Phu Tran <tr.ngocphu@gmail.com>
2020-04-23 22:31:51 +0800

    use paper size for both width and height

--- static/js/config.js
+++ static/js/config.js
@@ -9,7 +9,7 @@
 	65 Nanyang Drive, Nanyang Technological University,
 	North Spine, Block N3.2-B3M-10, 637460 Singapore
  * -----
- * Last Modified: Thursday, 23rd April 2020 10:30:07 pm
+ * Last Modified: Thursday, 23rd April 2020 10:31:00 pm
  * Modified By: Phu N. Tran
  * -----
  */
@@ -22,7 +22,6 @@ const PAPER_HEIGHT = PAPER_SIZE;
  
 
 /*************************************************/
-
 let canvas = document.getElementById('myCanvas');
 canvas.width = PAPER_WIDTH;
 canvas.height = PAPER_HEIGHT;
