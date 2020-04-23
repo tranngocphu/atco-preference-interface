9ec8e3ab6da611e2432b8f5064de1e1b283541cb
Phu Tran <tr.ngocphu@gmail.com>
2020-04-23 18:07:30 +0800

    add sample data structure

---
+++ static/js/class.js
@@ -0,0 +1,15 @@
+/*
+ * File: class.js
+ * File Path: /home/phu/projects/atco-preference-interface/static/js/class.js
+ * Project: js
+ * File Created: Thursday, 23rd April 2020 5:47:55 pm
+ * Author: Phu N. Tran (phutran@ntu.edu.sg, tr.ngocphu@gmail.com)
+ * Affiliation:
+ * 	Air Traffic Management Research Institute,
+	65 Nanyang Drive, Nanyang Technological University,
+	North Spine, Block N3.2-B3M-10, 637460 Singapore
+ * -----
+ * Last Modified: Thursday, 23rd April 2020 5:47:55 pm
+ * Modified By: Phu N. Tran
+ * -----
+ */
