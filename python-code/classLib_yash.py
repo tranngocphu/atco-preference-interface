cb5064d88aa1192f0319228123872a5258a4fbfb
Yash <43232867+yashGuleria@users.noreply.github.com>
2020-05-08 12:32:05 +0800

    Addition fo methods to Airways class

--- python-code/classLib_yash.py
+++ python-code/classLib_yash.py
@@ -12,7 +12,7 @@ class wayPoint:
     def __str__(self):
         return 'waypoint class:'+ self.name +'  x co-ord: '+ str(self.x) +'  y co-ord: '+str(self.y)
 
-class airways:
+class Airways:
     def __init__(self, name, points):
         self.name=name
         self.start_wp= points[0][0]
@@ -21,8 +21,44 @@ class airways:
         self.start_wp_y= points[0][1][1]
         self.end_wp_x= points[1][1][0]
         self.end_wp_y = points[1][1][1]
-    def __str__(self):
-        return 'airway name :'+  str(self.name) +' start point: '+  str(self.start_wp)+' end point: '+  str(self.end_wp)
+ 
+    def airwayStartLoc(self):
+        self.start_x = self.start_wp_x
+        self.start_y = self.start_wp_y
+        return np.array([self.start_x,self.start_y])
+
+    def airwayEndLoc(self):
+        self.end_x = self.end_wp_x
+        self.end_y = self.end_wp_y
+        return np.array([self.end_x,self.end_y])
+
+    def airwayCoefficient(self):
+        try:
+            self.slope = (self.end_wp_y - self.start_wp_y) / (self.end_wp_x - self.start_wp_x)
+        except ValueError:
+            self.slope= np.NaN
+        self.intercept = self.start_wp_y - self.start_wp_x*self.slope
+        
+        return self.slope, self.intercept
+        
+        """eqn- (y-y1)/(y2-y1) = (x-x1)/(x2-x1) --> y = ((y2-y1)/(x2-x1))*x - x1*((y2-y1)/(x2-x1)) +y1
+                                                Y = MX +C
+                                                so,Y(t) = M*X(t) +C, 
+                                                        where x = X(t) and Y(t) = ((y2-y1)/(x2-x1))*X(t) - x1*((y2-y1)/(x2-x1)) +y1      
+        """
+
+    def location(self):
+        self.xt_min = self.start_wp_x
+        self.xt_max = self.end_wp_x
+        self.slope = (self.end_wp_y - self.start_wp_y) / (self.end_wp_x - self.start_wp_x)
+        self.yt_min = self.xt_min*self.slope - self.start_wp_x*((self.end_wp_y - self.start_wp_y) / (self.end_wp_x - self.start_wp_x)) + self.start_wp_y
+        self.yt_max = self.xt_max*self.slope - self.start_wp_x*((self.end_wp_y - self.start_wp_y) / (self.end_wp_x - self.start_wp_x)) + self.start_wp_y
+        self.locations=[]
+        for i in np.arange(self.xt_min,self.xt_max,0.05):
+            yt = np.round(i,2)*self.slope - self.start_wp_x*((self.end_wp_y - self.start_wp_y) / (self.end_wp_x - self.start_wp_x)) + self.start_wp_y
+            self.locations.append([np.round(i,2),yt])
+
+        return self.locations
     
 
 
